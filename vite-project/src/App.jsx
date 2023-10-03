84498553186967d6fe7c61175008b4f1aa0e4e3d
unknown <kumawatrobin7@gmail.com>
2023-10-03 22:30:54 +0530

    second-commit

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,35 +1,50 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from "react";
+import axios from "axios";
 
-function App() {
-  const [count, setCount] = useState(0)
+function Add() {
+  const [name, setName] = useState("");
+  const [photo, setPhoto] = useState("");
 
+  function handleSubmit(e) {
+    e.preventDefault();
+    const formData = new FormData();
+    formData.append("name", name);
+    formData.append("photo", photo);
+    axios
+      .post("http://localhost:8080/category/add", formData, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      })
+      .then((result) => {
+        if (result.status === 201 && result.statusText === "Created")
+          console.log("Category Added Successfully");
+      });
+  }
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h2>Add Category</h2>
+      <form action="" method="post" onSubmit={handleSubmit}>
+        <label htmlFor="">Name</label>
+        <input
+          type="text"
+          placeholder="Category Name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <br />
+        <label htmlFor="">Photo</label>
+        <input
+          type="file"
+          name="photo"
+          id="photo"
+          onChange={(e) => setPhoto(e.target.files[0])}
+        />
+        <br />
+        <button type="submit">Add Category</button>
+      </form>
     </>
-  )
+  );
 }
 
-export default App
+export default Add;
--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import AddProduct from './AddProduct.jsx'
+import AddCategory from "./AddCategory.jsx";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./Home.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+ReactDOM.createRoot(document.getElementById("root")).render(
+<BrowserRouter>
+  <Routes>
+    <Route path="/" element={<Home />}></Route>
+    <Route path="/add" element={<AddProduct />}></Route>
+    <Route path="/product/add" element={<AddProduct />}></Route>
+    <Route path="/category/add" element={<AddCategory />}></Route>
+  </Routes>
+</BrowserRouter>
 )
